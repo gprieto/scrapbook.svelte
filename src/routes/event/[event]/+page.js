792901f03ee0fa77af651b9cfe10549497485339
Matt Russell <russell.d.matt@gmail.com>
2022-12-11 15:58:07 -0800

    added a non-working dynamic event route

---
+++ src/routes/event/[event]/+page.js
@@ -0,0 +1,6 @@
+export async function load() {
+  // Presumably we load the images here?
+  let images = [];
+
+  return { images };
+}
---
+++ src/routes/event/[event]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let data;
+  const { images } = data;
+</script>
+
+<h2>Event metadata:</h2>
+
+<h2>Image Gallery:</h2>
+{#each images as image}
+  <img src={image} alt="" />
+{/each}
+
+<style>
+  img {
+    max-width: 100%;
+  }
+</style>
