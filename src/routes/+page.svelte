787f14e0cf308b89c2692e429c219bc26ea88121
Matt Russell <russell.d.matt@gmail.com>
2022-12-11 16:05:19 -0800

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
 <h1>Welcome to a very unfinished scrapbook.svelte</h1>
 
 <a href="/test-markdown">A page using markdown</a>
+
+<h2>Events</h2>
+
+I'd like this list to be auto-generated.
+
+<ul>
+  <li><a href="/event/birthday">Birthday</a></li>
+  <li><a href="/event/valentines-day">Valentines day</a></li>
+</ul>
